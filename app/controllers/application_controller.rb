024b95fc312a4912ee8f4c48a621c35e5463e3de
Robert Hughes <roberts.ginger.email@gmail.com>
2018-06-14 21:56:28 +0100

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,17 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :authenticate_user
 
+  private
+
+  def authenticate_user
+    client_id = ENV['client_id']
+    redirect_uri = CGI.escape('http://localhost:3000/auth')
+    foursquare_url = "https://foursquare.com/oauth2/authenticate?client_id=#{client_id}&response_type=code&redirect_uri=#{redirect_uri}"
+    redirect_to foursquare_url unless logged_in?
+  end
+
+  def logged_in?
+    !!session[:token]
+  end
 end
--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,11 +1,9 @@
 class SearchesController < ApplicationController
-  def search
-  end
+  def search; end
 
   def foursquare
-
-    client_id = ENV['FOURSQUARE_CLIENT_ID']
-    client_secret = ENV['FOURSQUARE_SECRET']
+    client_id = ENV['client_id']
+    client_secret = ENV['client_secret']
 
     @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
       req.params['client_id'] = client_id
@@ -18,14 +16,22 @@ class SearchesController < ApplicationController
     body = JSON.parse(@resp.body)
 
     if @resp.success?
-      @venues = body["response"]["venues"]
+      @venues = body['response']['venues']
     else
-      @error = body["meta"]["errorDetail"]
+      @error = body['meta']['errorDetail']
     end
     render 'search'
+  rescue Faraday::TimeoutError
+    @error = 'There was a timeout. Please try again.'
+    render 'search'
+  end
 
-    rescue Faraday::TimeoutError
-      @error = "There was a timeout. Please try again."
-      render 'search'
+  def friends
+    resp = Faraday.get('https://api.foursquare.com/v2/users/self/friends') do |req|
+      req.params['oauth_token'] = session[:token]
+      # don't forget that pesky v param for versioning
+      req.params['v'] = '20160201'
+    end
+    @friends = JSON.parse(resp.body)['response']['friends']['items']
   end
 end
